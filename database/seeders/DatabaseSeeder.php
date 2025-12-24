4ddc9aaeba5ff5a38d268b109065175f4bb8f750
AwanWush <rhafhyalfarisy@gmail.com>
2025-12-24 10:03:11 +0700

    fix: conflict ProdukkSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,7 +19,7 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             KategoriProdukSeeder::class,
             MartSeeder::class,
-            ProdukSeeder::class,
+            // ProdukSeeder::class,
             BannerSeeder::class,
             ProdukssSeeder::class,
         ]);
--- database/seeders/ProdukSeeder.php
+++ database/seeders/ProdukSeeder.php
@@ -12,78 +12,78 @@ class ProdukSeeder extends Seeder
     /**
      * Run the database seeds.
      */
-    public function run(): void
-    {
-        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+    // public function run(): void
+    // {
+    //     DB::statement('SET FOREIGN_KEY_CHECKS=0;');
 
-        DB::table('produk')->truncate();      // Hapus data lama
-        DB::table('produk_mart')->truncate(); // hapus data tabel pivot many-to-many
+    //     DB::table('produk')->truncate();      // Hapus data lama
+    //     DB::table('produk_mart')->truncate(); // hapus data tabel pivot many-to-many
         
-        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+    //     DB::statement('SET FOREIGN_KEY_CHECKS=1;');
 
-        $produk1 = Produk::create([
-            'kategori_id' => 4, 
-            'nama_produk' => 'Potabee',
-            'deskripsi' => null,
-            'harga' => 8000,
-            'stok' => 31,
-            'gambar' => 'produk/product06.jpg',
-            'status_ketersediaan' => 'Tersedia',
-            'is_active' => true,
-            'persentase_diskon' => null
-        ]);
-        $produk1->marts()->attach([1, 2]); // Relasi ke mart
+    //     $produk1 = Produk::create([
+    //         'kategori_id' => 4, 
+    //         'nama_produk' => 'Potabee',
+    //         'deskripsi' => null,
+    //         'harga' => 8000,
+    //         'stok' => 31,
+    //         'gambar' => 'produk/product06.jpg',
+    //         'status_ketersediaan' => 'Tersedia',
+    //         'is_active' => true,
+    //         'persentase_diskon' => null
+    //     ]);
+    //     $produk1->marts()->attach([1, 2]); // Relasi ke mart
 
-        $produk2 = Produk::create([
-            'kategori_id' => 4, 
-            'nama_produk' => 'Chitato',
-            'deskripsi' => null,
-            'harga' => 9000,
-            'stok' => 20,
-            'gambar' => 'produk/product05.png',
-            'status_ketersediaan' => 'Tersedia',
-            'is_active' => true,
-            'persentase_diskon' => null
-        ]);
-        $produk2->marts()->attach([1, 3]);
+    //     $produk2 = Produk::create([
+    //         'kategori_id' => 4, 
+    //         'nama_produk' => 'Chitato',
+    //         'deskripsi' => null,
+    //         'harga' => 9000,
+    //         'stok' => 20,
+    //         'gambar' => 'produk/product05.png',
+    //         'status_ketersediaan' => 'Tersedia',
+    //         'is_active' => true,
+    //         'persentase_diskon' => null
+    //     ]);
+    //     $produk2->marts()->attach([1, 3]);
 
-        $produk3 = Produk::create([
-            'kategori_id' => 4, 
-            'nama_produk' => 'Pringles',
-            'deskripsi' => null,
-            'harga' => 17000,
-            'stok' => 13,
-            'gambar' => 'produk/product04.jpg',
-            'status_ketersediaan' => 'Tersedia',
-            'is_active' => true,
-            'persentase_diskon' => null
-        ]);
-        $produk3->marts()->attach([3]);
+    //     $produk3 = Produk::create([
+    //         'kategori_id' => 4, 
+    //         'nama_produk' => 'Pringles',
+    //         'deskripsi' => null,
+    //         'harga' => 17000,
+    //         'stok' => 13,
+    //         'gambar' => 'produk/product04.jpg',
+    //         'status_ketersediaan' => 'Tersedia',
+    //         'is_active' => true,
+    //         'persentase_diskon' => null
+    //     ]);
+    //     $produk3->marts()->attach([3]);
 
-        $produk4 = Produk::create([
-            'kategori_id' => 2, 
-            'nama_produk' => 'Cimory Squeee',
-            'deskripsi' => null,
-            'harga' => 7100,
-            'stok' => 47,
-            'gambar' => 'produk/product07.jpg',
-            'status_ketersediaan' => 'Tersedia',
-            'is_active' => true,
-            'persentase_diskon' => null
-        ]);
-        $produk4->marts()->attach([2]);
+    //     $produk4 = Produk::create([
+    //         'kategori_id' => 2, 
+    //         'nama_produk' => 'Cimory Squeee',
+    //         'deskripsi' => null,
+    //         'harga' => 7100,
+    //         'stok' => 47,
+    //         'gambar' => 'produk/product07.jpg',
+    //         'status_ketersediaan' => 'Tersedia',
+    //         'is_active' => true,
+    //         'persentase_diskon' => null
+    //     ]);
+    //     $produk4->marts()->attach([2]);
 
-                $produk4 = Produk::create([
-            'kategori_id' => 2, 
-            'nama_produk' => 'Cimory Squeee',
-            'deskripsi' => null,
-            'harga' => 7100,
-            'stok' => 47,
-            'gambar' => 'produk/product07.jpg',
-            'status_ketersediaan' => 'Tersedia',
-            'is_active' => true,
-            'persentase_diskon' => null
-        ]);
-        $produk4->marts()->attach([2]);
-    }
+    //             $produk4 = Produk::create([
+    //         'kategori_id' => 2, 
+    //         'nama_produk' => 'Cimory Squeee',
+    //         'deskripsi' => null,
+    //         'harga' => 7100,
+    //         'stok' => 47,
+    //         'gambar' => 'produk/product07.jpg',
+    //         'status_ketersediaan' => 'Tersedia',
+    //         'is_active' => true,
+    //         'persentase_diskon' => null
+    //     ]);
+    //     $produk4->marts()->attach([2]);
+    // }
 }
