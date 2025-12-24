3b823ae9ca3796defb86cfdcbdbcefa1a58b32c0
AwanWush <rhafhyalfarisy@gmail.com>
2025-12-24 14:06:48 +0700

    add: a helper file and registering it into laravel

---
+++ app/Helpers/NavHelper.php
@@ -0,0 +1,10 @@
+<?php
+
+if (! function_exists('navIconClass')) {
+    function navIconClass(bool $active): string
+    {
+        return $active
+            ? 'bg-[#E68757]/20 text-[#930014] ring-1 ring-[#DB4B3A]'
+            : 'text-black hover:text-[#DB4B3A] hover:bg-[#E7BD8A]/30';
+    }
+}
