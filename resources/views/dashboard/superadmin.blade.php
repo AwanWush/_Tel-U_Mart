795bbb6afc87e3a92c43901a3fc44a4b26e3804c
D-Mar18 <damararum06@gmail.com>
2026-01-02 08:56:47 +0700

    super admin

--- resources/views/dashboard/superadmin.blade.php
+++ resources/views/dashboard/superadmin.blade.php
@@ -1,14 +1,14 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+        <h2 class="font-semibold text-xl text-black-800 dark:text-black-200 leading-tight">
             Dashboard Super Admin
         </h2>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-center">
+            <div class="mt-4 bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-center bg-gradient-to-r from-red-600 to-red-800 rounded-lg">
                     <h3 class="text-3xl font-extrabold text-white tracking-wide">
                         Selamat datang, <strong>{{ Auth::user()->name }}</strong> 👑
                     </h3>
